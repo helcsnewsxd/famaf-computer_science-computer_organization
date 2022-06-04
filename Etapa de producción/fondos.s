abdaf9a93af3cb60cd4de5635a392463d1707422
juanbratti <100317321+juanbratti@users.noreply.github.com>
2022-06-04 10:46:14 -0300

    nubes

--- Etapa de producción/fondos.s
+++ Etapa de producción/fondos.s
@@ -393,4 +393,86 @@ Nube1:
     ldr x0,[sp],8
 
     ret
+    
+    .globl Nube2
+Nube2:
+// Argumentos: 1 punto (x1,x2)
+
+    centrox .req x1
+    centroy .req x2
+    radio .req x3
+
+    str x0,[sp,-8]!
+    str x1,[sp,-8]!
+    str x2,[sp,-8]!
+    str x3,[sp,-8]!
+    str x4,[sp,-8]!
+    str x11,[sp,-8]!
+    str x12,[sp,-8]!
+    str x30,[sp,-8]!
+
+    mov x11,centrox
+    mov x12,centroy
+
+    ldr x0,=NUBE_BLANCO
+    mov radio,6
+    add centroy,centroy,4
+    bl Pinta_circulo
+    sub centroy,centroy,4
+
+    ldr x0,=NUBE_GRIS
+    mov radio,11
+    add centroy,centroy,13
+    sub centrox,centrox,13
+    bl Pinta_circulo
+
+    mov x4, 8 // Contador para el ciclo
+    Nube_circulos_grises_2:
+        // Ciclo para hacer la base
+        add centrox,centrox,3
+        bl Pinta_circulo
+        sub x4,x4,1
+        cmp x4,0
+        b.ne Nube_circulos_grises_2
+
+    ldr x0,=NUBE_BLANCO
+    mov radio,6
+    add centrox,centrox,6
+    bl Pinta_circulo
+
+    sub centroy,centroy,3
+    bl Pinta_circulo
+    
+    sub centroy,centroy,1
+    sub centrox,centrox,5
+    bl Pinta_circulo
+
+    sub centroy,centroy,4
+    sub centrox,centrox,5
+    bl Pinta_circulo
+
+    add centroy,centroy,4
+    mov radio, 10
+    sub centrox,centrox,8
+    bl Pinta_circulo
+
+    mov radio, 8
+    sub centrox,centrox,12
+    bl Pinta_circulo
+
+    .unreq centrox
+    .unreq centroy
+    .unreq radio
+
+    ldr x30,[sp],8
+    ldr x12,[sp],8
+    ldr x11,[sp],8
+    ldr x4,[sp],8
+    ldr x3,[sp],8
+    ldr x2,[sp],8
+    ldr x1,[sp],8
+    ldr x0,[sp],8
+
+    ret
+
 
